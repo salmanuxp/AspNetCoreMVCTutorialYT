c2ef980ff1b962a95df82a21750c497e62f9034b
Salman <salmanuxp@gmail.com>
2022-12-11 03:59:13 +0600

    Add project files.

---
+++ Asp.NetCoreIntro/Controllers/TutorialController.cs
@@ -0,0 +1,22 @@
+﻿using Asp.NetCoreIntro.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCoreIntro.Controllers
+{
+    public class TutorialController : Controller
+    {
+        public IActionResult Index()
+        {
+            var tutorials = new List<Tutorial>
+            {
+                new Tutorial { Id = 1, Name = "C#", Description = "C# Tutorial"},
+                new Tutorial { Id = 2, Name = "Asp.Net Core" , Description = "Asp.Net Core Tutorial"}
+            };
+
+
+            ViewData["tutorials"] = tutorials;
+
+            return View();
+        }
+    }
+}
