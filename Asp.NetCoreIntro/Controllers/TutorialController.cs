25f2ba008d388695c30a6683b0f0a87eae2d05ba
Salman <salmanuxp@gmail.com>
2022-12-13 04:08:18 +0600

    Upto Lecture 5 Passing Data from Controller to View https://www.youtube.com/watch?v=PbPfd8eD_QQ&list=PLdHN14J7CHtZN7dY7_Uo7ybUQRElDbUMj&index=5&ab_channel=JustPickandLearn

--- Asp.NetCoreIntro/Controllers/TutorialController.cs
+++ Asp.NetCoreIntro/Controllers/TutorialController.cs
@@ -1,22 +1,51 @@
 ﻿using Asp.NetCoreIntro.Models;
+
+using Asp.NetCoreIntro.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Asp.NetCoreIntro.Controllers
 {
     public class TutorialController : Controller
+         
+ 
+
     {
+        [ViewData]
+        public string title { get; set; }
         public IActionResult Index()
         {
-            var tutorials = new List<Tutorial>
+
+
+            //var tutorials = new List<Tutorial>
+            //    {
+            //        new Tutorial { Id = 1, Name = "C#", Description = "C# Tutorial"},
+            //        new Tutorial { Id = 2, Name = "Asp.Net Core" , Description = "Asp.Net Core Tutorial"}
+            //    };
+
+            //    title = "Tutorial List";
+            //    //ViewData["tutorials"] = tutorials;
+
+            //    ViewBag.Tutorials = tutorials;
+            //    ViewData["name"] = "All about .Net...";
+
+
+            var newModel = new TutorialViewModel
             {
-                new Tutorial { Id = 1, Name = "C#", Description = "C# Tutorial"},
-                new Tutorial { Id = 2, Name = "Asp.Net Core" , Description = "Asp.Net Core Tutorial"}
+
+                tutorial = new Tutorial
+                {
+                    Id = 1,
+                    Name = "Name",
+                    Description = "Desc"
+
+                },
+
+                title = "new title ",
             };
 
 
-            ViewData["tutorials"] = tutorials;
-
-            return View();
+            //return View();
+            return View(newModel);
         }
     }
 }
---
+++ Asp.NetCoreIntro/ViewModels/TutorialViewModelcs.cs
@@ -0,0 +1,11 @@
+﻿using Asp.NetCoreIntro.Models;
+
+namespace Asp.NetCoreIntro.ViewModels
+{
+    public class TutorialViewModel
+    {
+
+        public Tutorial tutorial { get; set; }
+        public string title { get; set; }
+    }
+}
