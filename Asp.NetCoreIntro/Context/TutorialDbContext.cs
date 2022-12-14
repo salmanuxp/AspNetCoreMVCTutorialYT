f2eb094d97064c6d13040578b8f56b58419a78e0
Salman <salmanuxp@gmail.com>
2022-12-14 18:56:24 +0600

    Upto video 9

---
+++ Asp.NetCoreIntro/Context/TutorialDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Asp.NetCoreIntro.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Asp.NetCoreIntro.Context
+{
+    public class TutorialDbContext:DbContext 
+        
+    {
+        public  TutorialDbContext(DbContextOptions<TutorialDbContext> options): base
+            (options) {
+        }
+        public DbSet<Tutorial> Tutorials { get; set; }  
+
+    }
+}
